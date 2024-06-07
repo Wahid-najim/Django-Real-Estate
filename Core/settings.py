fccaf336b1d40e1348f14bf27154a0b3ca094133
Reagan Odhiambo <errorcode.locked@gmail.com>
2024-06-07 15:12:41 +0300

    Major update

--- Core/settings.py
+++ Core/settings.py
@@ -91,6 +91,7 @@ WSGI_APPLICATION = 'Core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    #defaults
     "default": {
         "ENGINE": os.getenv("db_engine"),
         "NAME": os.getenv("db_name"),
